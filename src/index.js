bb26c07d198ede6c04ba60e1f10f96848cde095a
Isaac Smith <isaachsmith.dev@gmail.com>
2018-02-12 11:32:59 -0600

    Create onClick behavior

--- src/index.js
+++ src/index.js
@@ -3,18 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
-  
   render() {
       return (
-        <button className="square" onClick={() => this.setState({value: 'X'})}>
-          {/* TODO */}
-          {this.state.value}
+        <button className="square" onClick={() => this.props.onClick()}>
+          {this.props.value}
         </button>
       );
     }
@@ -29,9 +21,20 @@ class Square extends React.Component {
     }
 
     renderSquare(i) {
-      return <Square value={this.state.squares[i]}/>;
+      return( 
+        <Square 
+          value={this.state.squares[i]}
+          onClick={() => this.handleClick(i)}
+        />
+      );
     }
   
+    handleClick(i){
+      const squares = this.state.squares.slice();
+      squares[i] = 'X';
+      this.setState({squares: squares});
+    }
+
     render() {
       const status = 'Next player: X';
   
